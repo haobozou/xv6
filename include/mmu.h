38c7fb5689ab3fe5e179a84718143e5c51218ee6
Haobo Zou <zouhaobo@hotmail.com>
2024-11-26 00:14:26 +1100

    Move MINADDR definition

--- include/mmu.h
+++ include/mmu.h
@@ -81,7 +81,6 @@ struct segdesc {
 #define NPDENTRIES 1024 // # directory entries per page directory
 #define NPTENTRIES 1024 // # PTEs per page table
 #define PGSIZE 4096 // bytes mapped by a page
-#define MINADDR PGSIZE
 
 #define PTXSHIFT 12 // offset of PTX in a linear address
 #define PDXSHIFT 22 // offset of PDX in a linear address
